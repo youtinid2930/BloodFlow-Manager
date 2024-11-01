c03b277b22fb37219a65312e543cb07e59f96b2c
youtind2930 <tinidyoussef2023@gmail.com>
2024-11-01 18:00:54 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/emergency-services'),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
