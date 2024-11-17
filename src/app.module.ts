e3023d093a642d649d1598e9d3f640707b7d91d0
MohamedAgdid <mohamed.agdid@hotmail.com>
2024-11-17 19:46:25 +0100

    correction dataname

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,20 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
 import { DonorsModule } from './donors/donors.module';
 import { RequestModule } from './requests/requests.module';
 import { HistoriqueModule } from './historique/historique.module';
-
-@Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/emergency-services'),
-    DonorsModule,
-    RequestModule,
-    HistoriqueModule,
-=======
-import { UserModule } from './user/user.module';
+import { DonationsModule } from './donations/donations.module';
 import { BloodStockModule } from './blood_stock/blood_stock.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/BloodFlow'),
-    UserModule,
+    DonorsModule,
+    DonationsModule,
     BloodStockModule,
->>>>>>> acb1728... creation of Blood stock
+    RequestModule,
+    HistoriqueModule,
   ],
   controllers: [AppController],
   providers: [AppService],
