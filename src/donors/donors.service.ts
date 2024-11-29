b10bdc1e5c171696f27cb9629c70de91f33c8ed9
MOHAMED AGDID <155784661+MohamedAgdid@users.noreply.github.com>
2024-11-29 22:42:35 +0100

    Merge pull request #2 from youtinid2930/stone
    donors db and crud test

--- src/donors/donors.service.ts
+++ src/donors/donors.service.ts
@@ -1,29 +1,57 @@
+import { DonorsModule } from './donors.module';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDonorDto } from './dto/create-donor.dto';
 import { UpdateDonorDto } from './dto/update-donor.dto';
+import { Model, Types } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Donor } from './schemas/donor.schema';
+import { Donor,DonorSchema } from './schemas/donor.schema';
 
 @Injectable()
 export class DonorsService {
-  create(createDonorDto: CreateDonorDto) {
-    return 'This action adds a new donor';
-  }
+  constructor(
+    @InjectModel(Donor.name) private donorModel: Model<Donor>,
+  ) {}
+  
 
+  // Create
+  async create(createDonorDto: CreateDonorDto): Promise<Donor> {
+      const newDonor = new this.donorModel(createDonorDto);
+      return await newDonor.save();
+    }
+  
   findAll() {
-    return `This action returns all donors`;
+    return this.donorModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} donor`;
+  findOne(id: any) {
+    return this.donorModel.findById(id).exec();
   }
 
-  update(id: number, updateDonorDto: UpdateDonorDto) {
-    return `This action updates a #${id} donor`;
+  async update(id: any, updateDonorDto: UpdateDonorDto): Promise<Donor> {
+    const donor = await this.donorModel.findById(id);
+
+    if (!donor) {
+      throw new NotFoundException('donor nexite pas');
+    }
+    const updateddonor = await this.donorModel.findByIdAndUpdate(id, updateDonorDto, { new: true }).exec();
+    //new true: return the updateed one document
+    
+    if (!updateddonor) {
+      throw new NotFoundException('Donor could not be updated');
+    }
+    
+    return updateddonor;
+
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} donor`;
+  async remove(id: any): Promise<Donor> {
+    const removedDonor = await this.donorModel.findByIdAndDelete(id).exec();
+  
+    if (!removedDonor) {
+      throw new Error('Donor nexiste');
+    }
+  
+    return removedDonor;
   }
 }
